ad8d5f4811b053430f9d033098aabbd8ea89b67d
GitHub Action <action@github.com>
2026-05-07 18:20:05 +0000

    Bump version to 2.11.27-mod2 (21102702)

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,8 +58,8 @@ subprojects {
             minSdk = 21
             targetSdk = 35
 
-            versionName = "2.11.27-mod1"
-            versionCode = 21102701
+            versionName = "2.11.27-mod2"
+            versionCode = 21102702
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
